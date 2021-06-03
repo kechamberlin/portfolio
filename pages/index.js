dbc7435efb34ebd1aad2860d66cde95d5e98675a
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-06-03 10:53:32 -0700

    Removed Image, styles from imports, added Fragment, About, Contact, FeaturedProjects, Hero, and getFeaturedProjects.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
+import { Fragment } from 'react';
+import Head from 'next/head';
+import About from '../components/home-page/about';
+import Contact from '../components/home-page/contact';
+import FeaturedProjects from '../components/home-page/featured-projects';
+import Hero from '../components/home-page/hero';
+import { getFeaturedProjects } from '../lib/projects-util';
+
   return (
     <div className={styles.container}>
       <Head>
