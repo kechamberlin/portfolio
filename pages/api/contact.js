4b521fbf3756224dc5bf64df1a8838147bc8eb49
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-06-04 15:37:52 -0700

    Created file for connecting to my mongo database, storing new messages in it, etc.

---
+++ pages/api/contact.js
@@ -0,0 +1,53 @@
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === ' ' ||
+      !message ||
+      message.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+    // Store in a database
+    const newMessage = {
+      name,
+      email,
+      message,
+    };
+
+    let client;
+
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.nglet.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
+    try {
+      client = await MongoClient.connect(connectionString);
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message FAILED!' });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: 'Successfully stored message!', message: newMessage });
+  }
+}
